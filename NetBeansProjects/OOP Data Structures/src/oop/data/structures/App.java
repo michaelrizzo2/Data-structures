e772afb27a148df752bbf7d9b24fe07a25e309c3
Michael <m_rizzo@live.com>
2017-06-17 00:52:53 +0000

    created app class to test stack implementation

---
+++ NetBeansProjects/OOP Data Structures/src/oop/data/structures/App.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop.data.structures;
+
+
+
+/**
+ *
+ * @author michael
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        stack my_stack =new stack(10);
+        my_stack.push(20);
+        my_stack.push(20);
+        my_stack.push(20);
+        my_stack.push(20);
+        my_stack.push(20);
+        my_stack.push(20);
+        my_stack.push(20);
+        my_stack.push(20);
+        my_stack.push(20);
+        my_stack.push(20);
+        
+        
+    }
+    
+}
