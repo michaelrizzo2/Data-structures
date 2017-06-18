f25a0873e8f10b5f1a4beb3890611358f5435895
Michael <m_rizzo@live.com>
2017-06-18 19:22:33 +0000

    Set up char version of java stack creator(stack class)

---
+++ NetBeansProjects/OOP Data Structures/src/oop/data/structures/stackchar.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop.data.structures;
+
+/**
+ *
+ * @author michael
+ */
+public class stackchar {
+    
+    private int max_size;
+    private long stackarray[];
+    private int top;
+    
+    //This wil set up the array
+    public stackchar(int size)
+    {
+        this.max_size=size;
+        this.stackarray=new long[max_size];
+        this.top=-1;
+    }
+    
+    public void push(long j)
+    {
+        if (!isFull())
+        {
+        top++;
+        stackarray[top]=j;
+        }
+        
+        else 
+        {
+            System.out.println("You have exceeded the bounds of the array");
+        }
+        
+        
+    }
+    
+    public long pop()
+    {
+        int old_top=top; 
+        top--;
+        return stackarray[old_top];  
+    }
+    
+    public long peek()
+    {
+        return stackarray[top];
+    }
+    
+    public boolean isEmpty(){
+        return top==-1;
+    }
+    
+    public boolean isFull()
+    {
+        return top==max_size-1;
+    }
+    
+    
+    
+    
+}
