42f73d0e6bc14c9322e2b2f785e9ab70947209fd
Michael <m_rizzo@live.com>
2017-06-18 22:15:04 +0000

    added logic to pop the elements of the string stack to reverse the string.

--- NetBeansProjects/OOP Data Structures/src/oop/data/structures/Appchar.java
+++ NetBeansProjects/OOP Data Structures/src/oop/data/structures/Appchar.java
@@ -15,6 +15,7 @@ public class Appchar {
         
         
         String reverse = reverser("String is string");
+        System.out.println(reverse);
         
 }
     
@@ -22,6 +23,8 @@ public class Appchar {
     {
         //first we need to get the length of the string to set up the stack
         int stack_size=string.length();
+        //This will be for the reversed string
+        String reversed_string="";
         //we need to set up the stack
         stackchar string_stack=new stackchar(stack_size);
         //now we need to push the elements onto the stack
@@ -30,7 +33,11 @@ public class Appchar {
             string_stack.push(string.charAt(i));           
         }
         
-        
-        return string;
+        //now we need to pop the elements onto the stack to reverse the string
+        while (!string_stack.isEmpty())
+        {
+            reversed_string+=string_stack.pop();
+        }
+        return reversed_string;
     }
 }
