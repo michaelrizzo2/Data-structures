c10282f266087c0ac98c174fd96857df54b9e763
Michael <m_rizzo@live.com>
2017-06-20 17:12:20 +0000

    started to set up variables for queue data structure

---
+++ NetBeansProjects/OOP Data Structures/src/oop/data/structures/queue.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop.data.structures;
+
+/**
+ *
+ * @author michael
+ */
+public class queue {
+    
+    //Setting up the variables
+    private int maxsize;//maintains set number of slots
+    private long[] queuearray;
+    private int front;
+    private int back;
+    private int nitems;//This will count the number of items
+    
+    
+}
