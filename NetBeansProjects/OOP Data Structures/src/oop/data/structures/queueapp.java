6f4095e9bb88b1aad9899d34b9c0560d4c38ca42
Michael <m_rizzo@live.com>
2017-06-21 17:44:21 +0000

    set up queue and inserted values into queue

---
+++ NetBeansProjects/OOP Data Structures/src/oop/data/structures/queueapp.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop.data.structures;
+
+/**
+ *
+ * @author michael
+ */
+public class queueapp {
+    
+    public static void main(String[] args) 
+    {
+        //create a new queue
+        queue myqueue=new queue(5);
+        myqueue.insert(1);
+        myqueue.insert(2);
+        myqueue.insert(3);
+        myqueue.insert(4);
+        myqueue.insert(5);
+        myqueue.insert(5);
+        
+        
+    }
+          
+    
+}
