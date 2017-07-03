f3075ba329498573f7d21b751511e6667b4c3a2e
Michael <m_rizzo@live.com>
2017-07-03 18:12:10 +0000

    added node class (including node constructor)

---
+++ NetBeansProjects/OOP Data Structures/src/oop/data/structures/Node.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop.data.structures;
+
+/**
+ *
+ * @author michael
+ */
+public class Node {
+    //Setting the porperties for the node
+    Node next=null;
+    int node_index,node_data;
+    //We will set up the constructor to initialize the node
+    public Node(Node node,int node_index)
+            {
+                this.node_index=node_index;
+                this.node_data=node_data;
+            }
+    
+}
